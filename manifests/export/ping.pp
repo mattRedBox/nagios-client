9643eadb4db464e77c4bc5c08428a5c6c16eb464
mattRedbox <matt@redboxresearchdata.com.au>
2014-02-05 00:30:45 +1000

    updated check_ping to match other resources.

--- manifests/export/ping.pp
+++ manifests/export/ping.pp
@@ -2,11 +2,16 @@ class nagios-client::export::ping {
     
   include nagios-client::defaults
 
+  export::update_config{"check_ping-${::fqdn}":
+    resource_specific => $::fqdn,
+    resource_generic  => 'check_ping',
+    nagios_resource   => 'service',
+  }
+
   @@nagios_service { "${::fqdn}-check_ping":
     use       => 'check_ping',
     host_name => $::fqdn,
     target    => "$defaults::resource_dir/check_ping/${::fqdn}.cfg",
-    ## require   => Nagios-server::Config_update[$::fqdn, 'service', 'check_ping'],
   }
 
 }
