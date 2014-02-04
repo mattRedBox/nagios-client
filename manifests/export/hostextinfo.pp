da10cfb26bc4acb525b575e1a95a3a23d3c6ba00
mattRedbox <matt@redboxresearchdata.com.au>
2014-02-05 00:06:52 +1000

    updated hostextinfo to match host exported resource setup.

--- manifests/export/hostextinfo.pp
+++ manifests/export/hostextinfo.pp
@@ -1,13 +1,16 @@
 class nagios-client::export::hostextinfo {
     
   include nagios-client::defaults
-  
+
+  export::update_config{$::fqdn:
+    nagios_resource => 'hostextinfo',
+  }
+
   @@nagios_hostextinfo { $::fqdn:
     ensure          => present,
     icon_image_alt  => $::operatingsystem,
     icon_image      => "base/${::operatingsystem}.png",
     statusmap_image => "base/${::operatingsystem}.gd2",
     target          => "$defaults::resource_dir/hostextinfo/${::fqdn}.cfg",
-    ##  require         => Nagios-server::Config_update[$::fqdn, 'hostextinfo'],
   }
 }
